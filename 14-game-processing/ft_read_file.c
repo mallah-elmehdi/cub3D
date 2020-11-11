1e3e5fff4d79dc35400f827a818641ddced640bf
mallah-elmehdi <elmehdimallah@gmail.com>
2020-11-11 20:57:44 +0100

    update

---
+++ 14-game-processing/ft_read_file.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_read_file.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emallah <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/03 19:17:10 by emallah           #+#    #+#             */
+/*   Updated: 2020/11/03 19:17:13 by emallah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int		ft_read_info(void)
+{
+	int		fd;
+	char	*line;
+
+	if ((fd = open(g_file_name, O_RDONLY)) == ERR)
+		return (ft_err("ERROR\nfile error: invalid file name / file not found"));
+	while ((g_gnl = get_next_line(fd, &line)))
+	{
+		if (ft_info_reader(line))
+			return (ERR);
+		if (!ft_is_info_done())
+			g_map_start++;
+		if (g_gnl == 2)
+			break ;
+	}
+	return (0);
+}
+
+int		ft_read_map(void)
+{
+	int		fd;
+	int		start;
+	char	*line;
+
+	start = 0;
+	if ((fd = open(g_file_name, O_RDONLY)) == ERR)
+		return (ft_err("ERROR\nfile error: invalid file name / file not found"));
+	while ((g_gnl = get_next_line(fd, &line)))
+	{
+		if (start == g_map_start)
+		{
+			if (ft_map_reader(line))
+				return (ERR);
+		}
+		else
+			start++;
+		if (g_gnl == 2)
+			break ;
+	}
+	return (0);
+}
+
+int		ft_read_file(void)
+{
+	if (ft_read_info())
+		return (ERR);
+	if (ft_height_width_map())
+		return (ERR);
+	g_map = (char **)malloc(sizeof(char *) * (g_map_height + 1));
+	g_map[g_map_height] = NULL;
+	if (ft_read_map())
+		return (ERR);
+	return (0);
+}
---
+++ 14-game-processing/mlx/ft_game.c
@@ -0,0 +1,52 @@
+#include "../cub3d.h"
+
+void ft_quit(void)
+{
+	mlx_destroy_image(g_mlx, g_mlx_img);
+	mlx_destroy_window(g_mlx, g_mlx_win);
+	exit(0);
+}
+
+void ft_update_img(int key_code)
+{
+	if (key_code == UP)
+        ft_move_up();
+	if (key_code == DOWN)
+        ft_move_down();
+	if (key_code == RIGHT)
+        ft_move_right();
+	if (key_code == LEFT)
+        ft_move_left();
+	// if (key_code == LEFT_ROT || key_code == RIGHT_ROT)
+    //     ft_rotate(key_code);
+	mlx_destroy_image(g_mlx, g_mlx_img);
+	ft_game_loop();
+}
+
+int ft_move_player(int key_code)
+{
+	if (key_code == ESC)
+		ft_quit();
+	if (ft_allowed_key(key_code))
+		ft_update_img(key_code);
+	mlx_hook(g_mlx_win, key_code, 1000, ft_move_player, g_param);
+	return (0);
+}
+
+void ft_game_loop(void)
+{
+	g_mlx_img = mlx_new_image(g_mlx, g_img.res[0], g_img.res[1]);
+	g_mlx_str = (int *)mlx_get_data_addr(g_mlx_img, &g_img.bpp, &g_img.size_line, &g_img.endian);
+	ft_game_init();
+	ft_draw_player();
+	mlx_put_image_to_window(g_mlx, g_mlx_win, g_mlx_img, 0, 0);
+}
+
+void ft_game(void)
+{
+	ft_game_var_init();
+	g_mlx = mlx_init();
+	g_mlx_win = mlx_new_window(g_mlx, g_infos.resolution[0], g_infos.resolution[1], "cub3d");
+	mlx_key_hook(g_mlx_win, ft_move_player, g_param);
+	ft_game_loop();
+}
