2c8b511ebc60ae52662347eba1d3f1796c418af5
mallah-elmehdi <elmehdimallah@gmail.com>
2020-11-24 20:44:16 +0100

    keep going...

---
+++ 16-game-processing/cub3d.h
@@ -0,0 +1,174 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emallah <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/19 19:28:06 by emallah           #+#    #+#             */
+/*   Updated: 2020/10/24 20:26:53 by emallah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# define ERR -1
+# define SZ 32
+# define SZ_PY 10
+
+# define RT_ROT 124
+# define LT_ROT 123
+# define UP 13
+# define DN 1
+# define LT 0
+# define RT 2
+# define ESC 53
+
+# define C_0 16777215
+# define C_1 7127551
+# define C_2 15103231
+# define C_PL 5954632
+
+// # define RIGHT_ROT 65363
+// # define LEFT_ROT 65361
+// # define UP 119
+// # define DOWN 115
+// # define LEFT 97
+// # define RIGHT 100
+// # define ESC 65307
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <math.h>
+# include <mlx.h>
+
+typedef struct	s_info
+{
+	char		*so_txt;
+	char		*we_txt;
+	char		*no_txt;
+	char		*ea_txt;
+	char		*s_txt;
+	int			rsl[2];
+	int			flr;
+	int			cel;
+}				t_info;
+
+typedef struct	s_img
+{
+	int			rsl[2];
+	int			bpp;
+	int			sz_ln;
+	int			e;
+}				t_img;
+
+typedef struct	s_mvt
+{
+	int			pd;
+	int			lr;
+	int			rot;
+	float		agl;
+}				t_mvt;
+
+typedef struct	s_ply
+{
+    float		x;
+    float		y;
+    float		wspd;
+    float		tspd;
+    float		wdrc;
+    float		tdrc;
+    float		agl;
+}				t_ply;
+
+void			*g_mlx;
+void			*g_mlx_win;
+void            *g_mlx_img;
+int             *g_mlx_str;
+
+void			*g_param;
+char			**g_map;
+char			*g_file_name;
+int				g_map_start;
+int				g_map_height;
+int				g_map_width;
+int				g_gnl;
+struct s_img	g_img;
+struct s_mvt	g_mvt;
+struct s_info	g_info;
+struct s_ply	g_ply;
+
+void			*ft_calloc(size_t type, size_t size);
+void			free_it(char **s);
+void			free_it_double(char **s, int counter);
+void			ft_strtrim(char **line);
+void			ft_strtrim_one_side(char **line);
+void			ft_dimension(char *line, int *x, int *y);
+void			ft_initilisation(char *file);
+void			free_it_double_int(int **s, int counter);
+char			**ft_split(char *s, char c);
+size_t			ft_strlen(char *str);
+int				check_out(char **str1, char **str2, int *i);
+int				fill_it(char **string1, char **string2, int *h, int *s);
+int				re_alloc(char **str);
+int				len(char *s);
+int				after_check(char **str1, char **str2, int *i);
+int				get_next_line(int fd, char **line);
+int				index_front(char *str);
+int				index_back(char *str);
+int				str_table_length(char *s, char c);
+int				string_length(char *s, char c);
+int				ft_atoi(char *s);
+int				ft_strlen_trim(char *str);
+int				ft_err(char *str);
+int				ft_read_file(void);
+int				ft_resolution_syntax(char *line);
+int				ft_resolution_errors(char *line);
+int				ft_resolution_add(char *line);
+int				ft_resolution(char *line);
+int				ft_info_reader(char *line);
+int				ft_color_syntax(char *line);
+int				ft_color_errors(char **rgb);
+int				ft_color(char *line, char c);
+int				ft_is_int(char c);
+int				ft_texture(char *line);
+int				ft_texture_str(char *line, char **texture, int start);
+int				ft_texture_add(char *line, char *txt);
+int				ft_is_info_done(void);
+int				ft_is_player(char c);
+int				ft_map_reader(char *line);
+int				ft_read_map();
+int				ft_read_info(void);
+int				ft_is_whitespace(char c);
+int				ft_is_map(char c);
+int				ft_height_width_map();
+int				ft_map_errors();
+int				ft_is_map_has_empty_line();
+int				ft_map_check_1(int i, int j);
+int				ft_map_check_2(int i, int j);
+int				ft_map_check_3(int i, int j);
+int				ft_check_map(void);
+// =====================================
+void			ft_game(void);
+void			ft_game_loop(void);
+void			ft_game_var_init(void);
+void			ft_draw_map(void);
+void			ft_ply_init(int i, int j);
+void			ft_draw_rect(int x, int y, int color);
+void			ft_draw_player(void);
+void			ft_draw_rays(void);
+void			ft_draw_rays_2(void);
+void			ft_update_img(int key_code);
+void			ft_quit(void);
+float			ft_y(int i);
+float			ft_cos(int o);
+float			ft_sin(int o);
+int				ft_crcl(float o, int r);
+int				ft_move_player(int key_code);
+int				ft_allowed_key(int key);
+
+
+#endif
---
+++ 16-game-processing/read_file/ft_resolution.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_resolution.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emallah <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/20 14:27:35 by emallah           #+#    #+#             */
+/*   Updated: 2020/10/25 11:57:17 by emallah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+int		ft_resolution_syntax(char *line)
+{
+	int		i;
+
+	i = 2;
+	if (!ft_is_whitespace(line[1]))
+		return (ft_err("ERROR\nresolution error: syntax error"));
+	while (line[i] != 0)
+	{
+		if (!ft_is_int(line[i]) && !ft_is_whitespace(line[i]))
+			return (ft_err("ERROR\nresolution error: syntax error"));
+		i++;
+	}
+	return (0);
+}
+
+int		ft_resolution_errors(char *line)
+{
+	int		i;
+	int		a;
+
+	i = 0;
+	a = 0;
+	while (line[i] != 0)
+	{
+		if (ft_is_int(line[i]))
+		{
+			a++;
+			while (ft_is_int(line[i]))
+			{
+				if (!(line[i + 1] >= 48 && line[i + 1] <= 57))
+					break ;
+				i++;
+			}
+		}
+		i++;
+	}
+	if (a != 2)
+		return (ft_err("ERROR\nresolution error: arguments error"));
+	return (0);
+}
+
+void	ft_dimension(char *line, int *x, int *y)
+{
+	int		i;
+
+	i = 0;
+	while (line[i] != 0)
+	{
+		if (*x == 0)
+		{
+			while (ft_is_int(line[i]))
+			{
+				if (*x < 99999)
+					*x = (*x * 10) + (line[i] - 48);
+				i++;
+			}
+		}
+		else if (*y == 0)
+		{
+			while (ft_is_int(line[i]))
+			{
+				if (*y < 99999)
+					*y = (*y * 10) + (line[i] - 48);
+				i++;
+			}
+		}
+		i++;
+	}
+}
+
+int		ft_resolution_add(char *line)
+{
+	int		x;
+	int		y;
+
+	x = 0;
+	y = 0;
+	ft_dimension(line, &x, &y);
+	if (x > 5120)
+		x = 5120;
+	if (y > 2880)
+		y = 2880;
+	g_info.rsl[0] = x;
+	g_info.rsl[1] = y;
+	return (0);
+}
+
+int		ft_resolution(char *line)
+{
+	if (g_info.rsl[0] != -1 && g_info.rsl[1] != -1)
+		return (ft_err("ERROR\nresolution error: doubled argument"));
+	if (ft_resolution_syntax(line))
+		return (ERR);
+	if (ft_resolution_errors(line))
+		return (ERR);
+	if (ft_resolution_add(line))
+		return (ERR);
+	if (g_info.rsl[0] == 0 || g_info.rsl[1] == 0)
+		return (ft_err("ERROR\nresolution error: couldn't creat a window with 0000x0000 resolution"));
+	return (0);
+}
